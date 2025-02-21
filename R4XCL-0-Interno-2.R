304dfa3098890e13c158aa9ee3b9c5860c70ec75
MINOR BONILLA GOMEZ <mbonillacr@hotmail.com>
2025-02-21 13:29:27 -0600

    Agregando Librerías

--- R4XCL-0-Interno-2.R
+++ R4XCL-0-Interno-2.R
@@ -11,25 +11,25 @@ R4XCL_INT_PROCEDIMIENTOS <- function()
       "[07] Test de Breush-Pagan para efectos aleatorios en [i]",
       "[08] Test de Breush-Pagan para efectos aleatorios en [t,i]",
       "[09] Test de Breush-Pagan para efectos aleatorios",
-      "[10] Test de Breush-Pagan para correlaci?n contemporanea",
-      "[11] Test de Pesaran para correlaci?n contemporanea",
+      "[10] Test de Breush-Pagan para correlación contemporánea",
+      "[11] Test de Pesaran para correlación contemporánea",
       "[12] Test de correlacion serial",
       "[13] Test de raiz unitaria", 
-      "[14] Especificaci?n empleada",
+      "[14] Especificación empleada",
       "[15] Guardar Modelo Estimado en RDS"
      )
   
   Reg_Lineal <- 
     c(
-      "[01] Estimar Modelo de Regresi?n Lineal",
+      "[01] Estimar Modelo de Regresión Lineal",
       "[02] Estimar valores para la [Y] (dentro de muestra)",
       "[03] Estimar valores para la [Y] (fuera de muestra)",
       "[04] Obtener efectos marginales",
-      "[05] Calcular coeficiente de Inflaci?n de Varianza VIF",
+      "[05] Calcular coeficiente de Inflación de Varianza VIF",
       "[06] Test de heterocedasticidad Breusch-Pagan", 
-      "[07] Estimaci?n robusta de coeficientes",
+      "[07] Estimación robusta de coeficientes",
       "[08] Identificar observaciones de influencia en la muestra",
-      "[09] Especificaci?n empleada",
+      "[09] Especificación empleada",
       "[10] Guardar Modelo Estimado en R",
       "[11] Modelo estimado con formato original de R",
       "[12] Obtener residuos estimados"
@@ -37,22 +37,22 @@ R4XCL_INT_PROCEDIMIENTOS <- function()
   
   Reg_Binaria <-  
     c(
-      "[1] Estimar Modelo de Regresi?n Binaria",
+      "[1] Estimar Modelo de Regresión Binaria",
       "[2] Estimar P[Y=1|XB] (dentro de muestra)",
       "[3] Estimar P[Y=1|XB] (fuera de muestra)",
       "[4] Test de Hosmer & Lemeshow",
       "[5] Obtener efectos marginales",
       "[6] ANOVA",
-      "[7] Especificaci?n empleada",
+      "[7] Especificación empleada",
       "[8] Guardar Modelo Estimado en R"
      )
   
   Reg_Tobit <-  
     c(
-      "[1] Estimar Modelo de Regresi?n Tobit",
+      "[1] Estimar Modelo de Regresión Tobit",
       "[2] Estimar [Y] (dentro de muestra)",
       "[3] Estimar [Y] (fuera de muestra)",
-      "[4] Especificaci?n empleada",
+      "[4] Especificación empleada",
       "[5] Guardar Modelo Estimado en R"
     )
   
@@ -70,7 +70,7 @@ R4XCL_INT_PROCEDIMIENTOS <- function()
         "[1] Estimar Modelo de Arboles de Decisi?n",
         "[2] Estimar [Y] (dentro de muestra)",
         "[3] Estimar [Y] (fuera de muestra)",
-        "[4] Especificaci?n empleada",
+        "[4] Especificación empleada",
         "[5] Guardar Modelo Estimado en R"
      )
   
@@ -80,7 +80,7 @@ R4XCL_INT_PROCEDIMIENTOS <- function()
       "[2] Centros",
       "[3] Variabilidad intra clases",
       "[4] Variabilidad Total",
-      "[5] Parametros de Proceso",
+      "[5] Parámetros de Proceso",
       "[6] Test GAP",
       "[7] K-Optimo"
     )
@@ -103,10 +103,10 @@ R4XCL_INT_PROCEDIMIENTOS <- function()
   
   Mat_Algebra <-  
     c(
-        "[1] Factorizaci?n de Cholesky",
+        "[1] Factorización de Cholesky",
         "[2] Valores propios",
         "[3] Vectores propios",
-        "[4] Decomposici?n QR",
+        "[4] Decomposición QR",
         "[5] Matriz Inversa",
         "[6] Singular Value Decomposition",
         "[7] Diagonal de la matriz",
@@ -178,11 +178,11 @@ R4XCL_INT_DIALOGOS <- function()
     SetDatosX.NS       = "Datos por Analizar",
     SetDatosX.MT       = "Seleccione la matriz cuadrada (incluir nombres de variables)",
     SetDatosY          = "Variables DEPENDIENTES",
-    Categorica         = "Contiene sus datos información NO NUMERICA? 1:SI, 0:NO",
+    Categorica         = "Contiene sus datos información NO NUMÉRICA? 1:SI, 0:NO",
     Filtro             = "0:Incluir registro, 1:Excluir registro (0:Default)",
-    Constante          = "Incluir constante de estimaci?n 1:SI, 0:NO (1:Default)",
+    Constante          = "Incluir constante de estimación 1:SI, 0:NO (1:Default)",
     Escala             = "Escalar datos? 1:SI, 0:NO (0:Default)",
-    Semilla            = "Valor de generaci?n aleatoria (ej:123456)",
+    Semilla            = "Valor de generación aleatoria (ej:123456)",
     Ponderadores       = "Pesos a emplear en Mínimos Cuadrados",
     K                  = "Cantidad de Clusters requeridos",
     Koptimo            = "Cantidad máxima de Clusters por testear",
@@ -201,10 +201,10 @@ R4XCL_INT_DIALOGOS <- function()
     TipoModelo.KM      = "1:Hartigan-Wong, 2:Lloyd, 3:Forgy, 4:MacQueen",
     TipoModelo.Binario = "0:Logit, 1:Probit",
     TipoOutput.Binario = "1:Modelo, 2:Probabilidad Estimada, 3:Predicción, 4:Test HL, 5:Efectos Marginales",
-    TipoOutput.Lineal  = "1:Modelo, 2:Y Estimado, 3:Predicción, 4:Efectos Marginales, 5: Multicolinealidad, 6: Heterocedasticidad, 7:Estimaci?n Robusta, 8: Outliers, 9:Especificacion, 10: Salvar Modelo",
-    TipoOutput.KM      = "1:Clasificaci?n, 2:Centros, 3:Variabilidad INTRA clase, 4:Variabilidad Total, 5:Parametros de Proceso, 6:GAP, 7:K-?ptimo",
-    TipoOutput.ACP     = "1:Matriz de Correlaci?n, 2:Coordenadas: Variables, 3:Coordenadas: Individuos, 4:COS^2: INDs, 5:Contribuci?n:INDs, 6:Valores Propios, 7:COS^2: VARs, 8:Contribución:VARs, 9:Predicci?n, 10:Gr?fico VARs|INDs",
-    TipoOutput.MT      = "1:Factorizaci?n Choleski, 2:Valores Propios, 3:Vectores Propios, 4:QR Decomposition, 5:Matriz Inversa, 6:Singular Value Decomposition, 7: Diagonal, 8:Transpuesta",
+    TipoOutput.Lineal  = "1:Modelo, 2:Y Estimado, 3:Predicción, 4:Efectos Marginales, 5: Multicolinealidad, 6: Heterocedasticidad, 7:Estimación Robusta, 8: Outliers, 9:Especificación, 10: Salvar Modelo",
+    TipoOutput.KM      = "1:Clasificación, 2:Centros, 3:Variabilidad INTRA clase, 4:Variabilidad Total, 5:Parámetros de Proceso, 6:GAP, 7:K-?ptimo",
+    TipoOutput.ACP     = "1:Matriz de Correlación, 2:Coordenadas: Variables, 3:Coordenadas: Individuos, 4:COS^2: INDs, 5:Contribución:INDs, 6:Valores Propios, 7:COS^2: VARs, 8:Contribución:VARs, 9:Predicción, 10:Gráfico VARs|INDs",
+    TipoOutput.MT      = "1:Factorización Choleski, 2:Valores Propios, 3:Vectores Propios, 4:QR Decomposition, 5:Matriz Inversa, 6:Singular Value Decomposition, 7: Diagonal, 8:Transpuesta",
     Variable_i         = "Variable que identifica a los individuos",
     Variable_t         = "Variable que identifica el tiempo",
     ValorTruncamiento  = "Valor umbral del truncamiento",   
